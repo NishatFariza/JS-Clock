d920ca9f4247d5bb6800a8648525ca7b2433611b
NishatFariza <nishatfarzia78@gmail.com>
2022-10-25 13:16:40 +0600

    title add

--- js/app.js
+++ js/app.js
@@ -1,28 +1,45 @@
-const hourHand = document.querySelector(".hour-hand")
-const minuteHand = document.querySelector(".minute-hand")
-const secondHand = document.querySelector(".second-hand")
+var hourHand = document.querySelector(".hour-hand")
+var minuteHand = document.querySelector(".minute-hand")
+var secondHand = document.querySelector(".second-hand")
+
+setInterval(setDate, 1000)
 
 function setDate() {
-    const now = new Date();
+    var now = new Date();
 
     //  second time 
-    const seconds = now.getSeconds();
-    const secondDegrees = ((seconds / 60) * 360) +90;
+    var seconds = now.getSeconds();
+    var secondDegrees = ((seconds / 60) * 360) +90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
     //Minute time
-    const minutes = now.getMinutes()
-    const minuteDegree = ((minutes / 60) * 360) + 90;
+    var minutes = now.getMinutes()
+    var minuteDegree = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
 
     //Hour time
-    const hour = now.getHours()
-    const hourDegree = ((hour / 12) * 360) + 90;
+    var hour = now.getHours()
+    var hourDegree = ((hour / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hourDegree}deg)`;
+
+
+    
+
+
+
 
 
 }
 
-setInterval(setDate, 1000)
+// const digitalClock = Document.querySelector(".digital-clock")
+// function digitalClock() {
+//     //digital clock time
+//     digitalClock.textContent = `${seconds} : ${minutes} : ${hour}`;
+//     }
+    
+// setDate()
 
-setDate()
+
+
+
+
