2f14b4e403c52384182aad05f0ebdde0be97a824
NishatFariza <nishatfarzia78@gmail.com>
2022-10-26 06:30:12 +0600

    add second audio

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
-var hourHand = document.querySelector(".hour-hand")
-var minuteHand = document.querySelector(".minute-hand")
-var secondHand = document.querySelector(".second-hand")
+const hourHand = document.querySelector(".hour-hand")
+const minuteHand = document.querySelector(".minute-hand")
+const secondHand = document.querySelector(".second-hand")
+const ticAudio = document.querySelector("audio")
 
 setInterval(setDate, 1000)
 
 function setDate() {
-    var now = new Date();
+    const now = new Date();
 
     //  second time 
-    var seconds = now.getSeconds();
-    var secondDegrees = ((seconds / 60) * 360) +90;
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds / 60) * 360) +90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
     //Minute time
-    var minutes = now.getMinutes()
-    var minuteDegree = ((minutes / 60) * 360) + 90;
+    const minutes = now.getMinutes()
+    const minuteDegree = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
 
     //Hour time
-    var hour = now.getHours()
-    var hourDegree = ((hour / 12) * 360) + 90;
+    const hour = now.getHours()
+    const hourDegree = ((hour / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hourDegree}deg)`;
 
 
-    
+    ticAudio.play();
+
+    //******************* Digital Clock **********************/
+    const ampm = hour >= 12
 
 
 
